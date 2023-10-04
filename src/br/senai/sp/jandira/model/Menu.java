66c85ba0f16cf9866b67609fadca5fba113137ae
JESSICA PEREIRA DOS SANTOS <23111734@CFP127ALUNO.LOCAL>
2023-10-04 15:52:58 -0300

    adicionar novo projeto zoo

---
+++ src/br/senai/sp/jandira/model/Menu.java
@@ -0,0 +1,20 @@
+package br.senai.sp.jandira.model;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    Scanner scanner = new Scanner(System.in);
+
+    public void executarMenu(){
+
+        boolean continuar = true;
+        do {
+            System.out.println("***************************** Menu *****************************");
+            System.out.println(" 1- listar animais ");
+            System.out.println(" 2- Cadastrar um novo animal ");
+            System.out.println(" 3- Habitats ");
+
+        }
+    }
+}
